9d4138b9fcc127f0272d8398726e3556bd0524d2
miatran1109 <miatran1109@gmail.com>
2021-01-22 17:54:13 +0700

    Prac 15: «Upgrade» your previous

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -8,6 +8,8 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
@@ -22,12 +24,16 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -35,16 +41,6 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-
-//        WeatherFragment weatherFragment = new WeatherFragment();
-//
-//        getSupportFragmentManager().beginTransaction()
-//                .add(android.R.id.content, weatherFragment).commit();
-//
-//        ForecastFragment forecastFragment = new ForecastFragment();
-//
-//        getSupportFragmentManager().beginTransaction()
-//                .add(android.R.id.content, forecastFragment).commit();
 
         PagerAdapter adapter = new HomeFragmentPagerAdapter(
                 getSupportFragmentManager());
@@ -70,38 +66,7 @@ public class WeatherActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_refresh:
             {
-                AsyncTask<String, Integer, Bitmap> task = new AsyncTask<String, Integer, Bitmap>() {
-                    @Override
-                    protected void onPreExecute() {
-                        // do some preparation here, if needed
-                    }
-                    @Override
-                    protected Bitmap doInBackground(String... params) {
-                        // This is where the worker thread's code is executed
-                        // params are passed from the execute() method call
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onProgressUpdate(Integer... values) {
-                        // This method is called in the main thread, so it's possible
-                        // to update UI to reflect the worker thread progress here.
-                        // In a network access task, this should update a progress bar
-                        // to reflect how many percent of data has been retrieved
-                    }
-                    @Override
-                    protected void onPostExecute(Bitmap bitmap) {
-                        // This method is called in the main thread. After #doInBackground returns
-                        // the bitmap data, we simply set it to an ImageView using ImageView.setImageBitmap()
-                        //                // Assume that we got our data from server
-                        Toast.makeText(getApplicationContext(), "some sample json here", Toast.LENGTH_SHORT).show();
-                    }
-                };
-                task.execute("http://ict.usth.edu.vn/wp-content/uploads/usth/usthlogo.png");
+                new task().execute();
                 return true;
             }
             case R.id.action_settings:
@@ -116,5 +81,38 @@ public class WeatherActivity extends AppCompatActivity {
         return false;
     }
 
+    private class task extends AsyncTask<URL, Void, Bitmap>{
+        Bitmap bitmap;
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+
+            try {
+                URL url = new URL("https://usth.edu.vn/uploads/logo_1_vi_1.png");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                int response = connection.getResponseCode();
+                Log.i("USTH Weather", "The response is: "+response);
+
+                InputStream is = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(is);
+
+                connection.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap){
+            ImageView logo = (ImageView) findViewById(R.id.logo);
+            logo.setImageBitmap(bitmap);
+        }
+    }
 }
 
